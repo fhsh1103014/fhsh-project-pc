6c9d8811663ef772197ed884c129a1ad57ed003e
Shinko <11030133@fhsh.tp.edu.tw>
2022-07-13 15:46:47 +0800

    first-time

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 // const colors = require('tailwindcss/colors')
 
 module.exports = {
-  content: ['index.html','./src/**/*.{js,jsx,ts,tsx,vue,html}'],
+  content: [
+    "index.html",
+    "work.html",
+    "profile-pc.html",
+    "./src/**/*.{js,jsx,ts,tsx,vue,html}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
